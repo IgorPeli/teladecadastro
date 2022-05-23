dafe860f316673141ca2d9eff7953085f7fc09eb
igorpeli <103079100+IgorPeli@users.noreply.github.com>
2022-05-23 18:29:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+init()
+
+function init(){
+    document.getElementById("nome").addEventListener("keyup", prox)
+    document.getElementById("cpf").addEventListener("keyup", prox)
+    document.getElementById("email").addEventListener("keyup", prox)
+    document.getElementById("cep").addEventListener("keyup", prox)
+    document.getElementById("senha").addEventListener("keyup", prox)
+  
+ 
+}
+
+function prox() {
+        var nome = document.getElementById("nome").value
+        var cpf = document.getElementById("cpf").value
+      var email = document.getElementById("email").value
+        var cep = document.getElementById("cep").value
+      var senha = document.getElementById("senha").value
+      
+      
+      localStorage.setItem("nome", nome)
+        localStorage.setItem("cpf", cpf)
+        localStorage.setItem("email", email)
+        localStorage.setItem("cep", cep)
+        localStorage.setItem("senha", senha)
+      }
